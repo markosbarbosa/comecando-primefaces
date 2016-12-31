0614f7a59e63d371d06edf4b4309a9fbd79a07e0
Marcos Barbosa <barbosa.mg@gmail.com>
2016-12-31 20:49:41 -0200

    Implementando seleção de linha da tabela Passando dados da linha ao selecioná-la Habilitando os botões de editar e excluir ao selecionar uma linha e desabilitando os mesmos botões ao desseclecionar a linha

--- src/main/java/com/algaworks/erp/controller/GestaoEmpresasBean.java
+++ src/main/java/com/algaworks/erp/controller/GestaoEmpresasBean.java
@@ -33,6 +33,7 @@ public class GestaoEmpresasBean implements Serializable {
     
     private List<Empresa> todasEmpresas;
     private Empresa empresaEdicao = new Empresa();
+    private Empresa empresaSelecionada;
     
     
     public void prepararNovoCadastro() {
@@ -67,6 +68,14 @@ public class GestaoEmpresasBean implements Serializable {
 
     public void setEmpresaEdicao(Empresa empresaEdicao) {
         this.empresaEdicao = empresaEdicao;
+    }
+
+    public Empresa getEmpresaSelecionada() {
+        return empresaSelecionada;
+    }
+
+    public void setEmpresaSelecionada(Empresa empresaSelecionada) {
+        this.empresaSelecionada = empresaSelecionada;
     }
     
     
